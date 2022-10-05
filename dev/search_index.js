83cc1e8460f4ca674b70538f92eb5a7b18db8efe
Documenter.jl <documenter@juliadocs.github.io>
2022-10-05 03:44:02 +0000

    build based on d9e51de

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PhyNE.jl","page":"PhyNE.jl","title":"PhyNE.jl","text":"","category":"section"},{"location":"","page":"PhyNE.jl","title":"PhyNE.jl","text":"Documentation for PhyNE.jl","category":"page"}]
+}
