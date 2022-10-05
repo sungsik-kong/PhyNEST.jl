5f302a71a4d82485afbc0d74a2ec94cd000b5c2a
Sungsik Kong <sungsik.kong@gmail.com>
2022-10-04 20:46:16 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,10 @@ makedocs(
     modules = [PhyNE]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
 deploydocs(
     repo = "github.com/sungsik-kong/PhyNE.jl.git"
 )
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
