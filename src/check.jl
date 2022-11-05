0f85cb0b8ba5d66ff4cc253884ded69ae4299075
Sungsik Kong <sungsik.kong@gmail.com>
2022-11-05 18:41:19 -0400

    Update check.jl

--- src/check.jl
+++ src/check.jl
@@ -1,6 +1,8 @@
 function greet()
     now=Dates.now()
-    msg=println("Thank you for using PhyNEST! Current time is $now.")
+    msg=println("Thank you for using PhyNEST: Phylogenetic Network Estimation using SiTe patterns.
+Please report bugs or make suggestions to https://groups.google.com/g/phynest-users.
+Current time is $now.")
     return msg
 end
 
